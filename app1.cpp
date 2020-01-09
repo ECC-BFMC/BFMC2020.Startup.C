cc5fd4992560096fd1f67630803cb16ab0a56d06
Nandor Kilyen <Nandor.Kilyen2@ro.bosch.com>
2020-01-09 14:47:29 +0200

    Add callback examples to app1

--- app1.cpp
+++ app1.cpp
@@ -7,6 +7,10 @@
 
 
 
+void print(std::string str){
+	std::cout<<str<<std::endl;
+}
+
 int main(int argc, char* argv[])
 {
 	try
@@ -15,10 +19,20 @@ int main(int argc, char* argv[])
 		ResponseHandler  	l_responseHandler;
 		// Create a communication manager object
 		SerialComManager 	l_communicationManager(l_responseHandler);
+
+
+		// Create a callback object for response handling
+		ResponseHandler::CallbackFncPtrType l_callbackFncObj=ResponseHandler::createCallbackFncPtr(&print);
+		// Attach object to Move and Brake response
+		l_responseHandler.attach(message::MCTL,l_callbackFncObj);
+		l_responseHandler.attach(message::BRAK,l_callbackFncObj);
 		// Create a move object
 		CMoveExample		l_moveObj(l_communicationManager);
 		// Run the move object 
 		l_moveObj.run();
+
+		l_responseHandler.detach(message::MCTL,l_callbackFncObj);
+		l_responseHandler.detach(message::BRAK,l_callbackFncObj);
 		// Close all ports and threads
 		l_communicationManager.closeAll();
 	}
--- src/example1.cpp
+++ src/example1.cpp
@@ -7,7 +7,7 @@ CMoveExample::CMoveExample(SerialComManager& f_comManager)
 
 void CMoveExample::run(){
     
-    std::array<float,9> l_pwmMotor_values={7.0,8.0,9.0,10.0,11.0,10.0,9.0,8.0,7.0};
+    std::array<float,9> l_pwmMotor_values={0.07,0.08,0.09,0.1,0.11,0.1,0.09,0.08,0.07};
     float l_steeringAngle=0.0;
 
     for (unsigned int i=0;i<l_pwmMotor_values.size();++i){
